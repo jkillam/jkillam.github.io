0497f892029a97a27e4ce32fe22e958c11d70878
Jonah Killam <>
2019-01-31 22:55:37 -0800

    Fixed auto scrolling, added favicons

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ $(document).ready(function(){
 
 
 
-
+    var is_running = false;
 
     // Select all links with hashes
     $('.navbar-nav a[href*="#"]')
@@ -93,6 +93,9 @@ $(document).ready(function(){
     .not('[href="#"]')
     .not('[href="#0"]')
     .on('click',function(event) {
+
+    console.log(is_running)
+
     // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
@@ -106,20 +109,35 @@ $(document).ready(function(){
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top-50
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+
+        // If we are close enough then don't do anything
+        if (Math.abs($(window).scrollTop() - target.offset().top) > 10) {
+
+          if (is_running) {
+            console.log('breaking')
+            return;
+          }
+          is_running = true;
+
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              console.log('setting false')
+              is_running = false;
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+            console.log('setting false')
+            is_running = false;
+          });
+        }
       }
     }
     });
